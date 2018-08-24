fd3b452faf3607cefbbb4c3e891a844a29d5cea7
DUSTIN MARTIN <dustin.martin@ad.infosys.com>
2018-08-24 10:15:40 -0400

    somewhat working library

--- GifLibraryFrontEnd/src/app/app.module.ts
+++ GifLibraryFrontEnd/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { GifLibraryComponent } from './gif-library/gif-library.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    GifLibraryComponent
   ],
   imports: [
     BrowserModule
